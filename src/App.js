2b5b62482bbb5121a869eca4455c431024eb7db2
Oksana0709 <o07091980@gmail.com>
2023-11-20 20:28:16 +0300

    [second]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-    
+    <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+ return (
+    <div className="header">
+<img src="/images/logo/Cream_royal.logo.svg" alt="LOGO" />
+    </div>
+ )
+
+  
+}
+
+export default Header;
