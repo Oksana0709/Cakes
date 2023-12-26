0055a12fa3abe181eba6d30986ab4ad49498264e
Oksana0709 <o07091980@gmail.com>
2023-12-26 18:35:37 +0300

    [card]

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from './Card.module.scss';
 
 const Card = ({title, price, image}) => {
+    const [cakeCount, cakeSetCount] = React.useState(0);
+    const onClickAdd = () => {
+        cakeSetCount(cakeCount + 1)
+    }
     return ( 
         <div className={styles.cakes}>
      <div className={styles.card}>
@@ -9,13 +13,26 @@ const Card = ({title, price, image}) => {
         <img src="" alt="Favorite" />
     </div>
          <img className={styles.img__card}src={image} alt="Cake" />
+         <div>
+            <ul>
+                <li className="active">26 sm</li>
+                <li className="active">30 sm</li>
+                <li className="active">40 sm</li>
+            </ul>
+         </div>
            <h4 className="">{title}</h4>
-           <div>
-            <div>
-              <span>{price}</span>
+           <div className="cake__block__bottom">
+            <div cake__block__price>
+              <span> from {price}</span>
               <b> dollars</b>
            </div>
-            <button className={styles.card__buttom}>Add</button>
+           <div> 
+           <button className={styles.card__buttom} onClick={onClickAdd}>
+            <span>+ Add</span>
+            <i>{cakeCount}</i>
+            </button>
+           </div>
+        
             </div>
          </div>
  
